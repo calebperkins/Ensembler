ed3fae0f93e88102654d16bcce128891fa6ff809
Jianneng Li <jl982@cornell.edu>
2011-10-21 22:26:09 -0400

    merging

--- EnsemPro/EnsemPro/MovementEvaluator.cs
+++ EnsemPro/EnsemPro/MovementEvaluator.cs
@@ -21,41 +21,42 @@ namespace EnsemPro
         /*Returns a floating number 0 to 1 which indicates how well the input is matching the movement */
         public float Accuracy(Movement m, ICollection<InputState> inputs, GameTime t)
         {
-            int totalInput = inputs.Count;
-            int correct = 0;
-
-            if (totalInput < 20) // if not many inputs, player hasn't moved that much => FAIL
+            switch (currentMovement.myType)
             {
-                return 0.00005f;
-            }
-            else
-            {
-                switch (currentMovement.myType)
-                {
-                    case Movement.Types.Shake:
-                        foreach (InputState state in inputs)
+                case Movement.Types.Shake:
+                    int scoreCounter = 0;
+                    int totalCounter = 0;
+                    foreach (InputState state in inputs)
+                    {
+                        if (Math.Abs(state.acceleration.X) > ACC_THRESHOLD || Math.Abs(state.acceleration.Y) > ACC_THRESHOLD)
                         {
-                            if (Math.Abs(state.acceleration.X) > ACC_THRESHOLD || Math.Abs(state.acceleration.Y) > ACC_THRESHOLD)
-                            {
-                                correct++;
-                            }
+                            scoreCounter++;
                         }
-                        return (float)correct / totalInput;
-                    case Movement.Types.Wave:
-                        Function f = currentMovement.f;
-                        foreach (InputState input in inputs)
-                        {
-                            if (totalInput >= f.Slopes.Length)
-                                break;
-                            float dist = Vector2.Distance(f.Positions[totalInput], input.position);
-                            if (dist < 150)
-                                correct++;
-                        }
-                        return correct / (float)totalInput;
+                        totalCounter++;
+                    }
+                    Debug.WriteLine("score counter is " + scoreCounter + "\n total counter is " + totalCounter);
 
-                    default: // no movement
-                        return 0.0f;
-                }
+                    // if fewer than 20 moves, NOT SHAKING! => fail
+                    return (totalCounter < 20 ? 0.00005f : ((float)scoreCounter / (float)totalCounter));
+                case Movement.Types.Wave:
+                    Function f = currentMovement.f;
+                    int count = 0;
+                    int correct = 0;
+
+                    //Debug.Assert(input.Count == f.Slopes.Length);
+
+                    foreach (InputState input in inputs)
+                    {
+                        if (count >= f.Slopes.Length)
+                            break;
+                        float dist = Vector2.Distance(f.Positions[count], input.position);
+                        if (dist < 150)
+                            correct++;
+                        count++;
+                    }
+                    return correct / (float)count;
+                default:
+                    return 0.0f;
             }
         }
 
