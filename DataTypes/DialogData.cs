adc42ad57a218569489872e933241b75c77f2b37
wc373 <wc373@cornell.edu>
2011-11-12 00:28:53 -0500

    Dialogs are now loaded from xml. Press Q to cancel any dialog.

---
+++ DataTypes/DialogData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.Xna.Framework.Content;
+
+namespace DataTypes
+{
+    public enum Locations
+    {
+        TestCity
+    }
+
+    public struct NewlyUnlockedSummary
+    {
+        public string Character;
+        public string Line;
+    }
+
+    public struct UnlockedSummary
+    {
+        public string Character;
+        public string Line;
+    }
+
+    public struct ClearedSummary
+    {
+        public string Character;
+        public string Line;
+    }
+
+    public class DialogData
+    {
+        public Locations Location;
+        public NewlyUnlockedSummary[] NewlyUnlocked;
+        public UnlockedSummary[] Unlocked;
+        public ClearedSummary[] Cleared;
+    }
+}
---
+++ EnsemPro/EnsemPro/DialogModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
+
+namespace EnsemPro
+{
+    public class DialogModel
+    {
+        private string DialogFileName;
+
+        public DataTypes.Locations Location
+        {
+            get;
+            set;
+        }
+
+        public DataTypes.NewlyUnlockedSummary[] NewlyUnlocked
+        {
+            get;
+            set;
+        }
+
+        public DataTypes.UnlockedSummary[] Unlocked
+        {
+            get;
+            set;
+        }
+
+        public DataTypes.ClearedSummary[] Cleared
+        {
+            get;
+            set;
+        }
+
+        public DialogModel(string filename)
+        {
+            DialogFileName = filename;
+        }
+
+        public void LoadContent(ContentManager cm)
+        {
+            DataTypes.DialogData data = cm.Load<DataTypes.DialogData>("Dialogs//"+DialogFileName);
+            Location = data.Location;
+            NewlyUnlocked = data.NewlyUnlocked;
+            Unlocked = data.Unlocked;
+            Cleared = data.Cleared;
+        }
+    }
+}
