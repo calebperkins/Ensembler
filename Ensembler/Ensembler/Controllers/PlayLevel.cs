793493b04570d900045af86750e383cc7dee7640
Shuyan Qin <shuyanqin@gmail.com>
2011-12-10 14:49:47 -0500

    fix 1812

--- Ensembler/Ensembler/Controllers/PlayLevel.cs
+++ Ensembler/Ensembler/Controllers/PlayLevel.cs
@@ -13,7 +13,7 @@ namespace Ensembler
     public class PlayLevel : DrawableGameComponent
     {
         // For adjusting curMaxAge of satisfaction queue
-        public const int AGE_DECR = 2;
+        public const int AGE_DECR = 0;
         public const int AGE_INCR = 1;
         bool started;
         bool failed;
@@ -354,6 +354,7 @@ namespace Ensembler
                     }
                 }
             }
+
 
             if (actionList.Count == 0 || failed) 
             {
